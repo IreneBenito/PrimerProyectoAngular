3b51c97245ebec9c680e003327b0cccd5ed0c9c6
Irene <UsuarioPC@MAKIWAZOWSKI>
2022-11-21 17:14:33 +0100

    missing files and uncommented data

--- src/main.ts
+++ src/main.ts
@@ -1,15 +1,15 @@
-/* import { enableProdMode } from '@angular/core'; */
+import { enableProdMode } from '@angular/core';
 import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 
 /* AppModule es la parte principal de la aplicacion, es donde estan definidas todas las importaciones, declaraciones de componentes , todo lo que va a utilizar el proyecto, a demas declara cual es el componente inicial*/
 import { AppModule } from './app/app.module';/* app.module.ts */
-/* import { enviroment } from './environments/environment'; */ /* me falta esta carpeta dentro del src */
+import { environment } from './app/environments/environment';
 
 /* para habilitar el modo produccion */
 /* configuracion para luego hacer los despliegues de manera mas optima */
-/* if(environment.prodution){
+if(environment.production){
   enableProdMode();
-} */
+} 
 
 /* esta parte es la mas importante */
 platformBrowserDynamic().bootstrapModule(AppModule)
