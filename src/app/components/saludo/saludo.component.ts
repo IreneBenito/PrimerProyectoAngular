68b08862a74c0ef6c2331f03063b0dd3284e7cf4
Irene <UsuarioPC@MAKIWAZOWSKI>
2022-12-05 12:50:41 +0100

    comments,OnInit and calling the component from app

--- src/app/components/saludo/saludo.component.ts
+++ src/app/components/saludo/saludo.component.ts
@@ -1,10 +1,25 @@
-import { Component } from '@angular/core';
+//interface OnInit es un hook, un punto del ciclo de vida del componente que permite realizar tareas cuando el componente este listo para ser renderizado
+import { Component , OnInit } from '@angular/core';
 
 @Component({
-  selector: 'app-saludo',
+  selector: 'app-saludo', //selector para poder declarar el componente en otro y anidarlos
   templateUrl: './saludo.component.html',
   styleUrls: ['./saludo.component.scss']
 })
-export class SaludoComponent {
+
+export class SaludoComponent implements OnInit {
+
+  //creamos variable nombre para llamar desde el componente con {{}}
+  nombre: string = "Irene";
+
+  //al constructor se le pueden añadir dependencias y providers
+  constructor() {
+  }
+
+  // asegurarse que ngOnInit está DESPUES del constructor
+  ngOnInit() {
+    //tareas previas a la renderizacion del componente .Ej.: peticion http
+    console.log("ngOnInit del componente Saludo");
+  }
 
 }
